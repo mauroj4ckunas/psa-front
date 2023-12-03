fab94cdf7f26e3909f7471c7c99499d3349ef953
iazuaje <iazuaje@fi.uba.ar>
2023-12-03 20:47:00 -0300

    Agregado de footer y header navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className=' w-full mb-10 flex justify-around text-black font-bold'>
-          <Link href={'/'}>PSA</Link>
-          <Link href={'/soporte'}>Soporte</Link>
-          <div>Proyecto</div>
+        <header className=' w-full flex justify-around text-black font-bold'>
+        <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-gray-300 shadow sm:items-baseline w-full">
+            <div className="mb-2 sm:mb-0">
+                <a href={'/'} className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">PSA</a>
+                <a href={'/soporte'} className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Soporte</a>
+                <a href={'/proyecto'} className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Proyectos</a>
+            </div>
+        </nav>
         </header>
-        {children}
+        <div className='h-full'>
+            {children}
+        </div>
+        <footer className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+            <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+            © 2023 Copyright: PSA
+            </div>
+        </footer>
       </body>
     </html>
   )
